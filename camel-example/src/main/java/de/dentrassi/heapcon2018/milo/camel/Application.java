7d5fdcf5a31b952134458374d515d58b5c1e7415
Jens Reimann <jreimann@redhat.com>
2018-10-10 15:27:02 +0200

    Restructure camel and add new server value

--- camel-example/src/main/java/de/dentrassi/heapcon2018/milo/camel/Application.java
+++ camel-example/src/main/java/de/dentrassi/heapcon2018/milo/camel/Application.java
@@ -15,10 +15,8 @@ import org.apache.camel.spring.boot.CamelSpringBootApplicationController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:main.xml")
 public class Application {
     public static void main(final String[] args) {
         ApplicationContext applicationContext = new SpringApplication(Application.class).run(args);
